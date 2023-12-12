99b979a1ba04d3cdc90e7ef886f4769b3dae3d91
DarkMatter-999 <darkmatter999official@gmail.com>
2023-12-12 14:03:52 +0530

    Added file opening and basic navigation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+use std::env;
 use next::terminal::Terminal;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
     let mut terminal = Terminal::new();
+
+    if args.len() > 1 {
+        if let Err(e) = terminal.open_editor(args[1].clone()) {
+            eprintln!("Error reading file: {}\n {}", args[1], e);
+        }
+    } else {
+        terminal.open_empty_editor();
+    }
 
     terminal.run();
 }
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,6 +1,9 @@
 use std::io;
 use std::io::Write;
 use std::process::exit;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufRead;
 
 use crossterm::terminal::{enable_raw_mode, size};
 // use crossterm::{execute, cursor::Show};
@@ -17,6 +20,10 @@ pub struct Terminal {
     size: (u16, u16),
     cursor: Cursor, 
     input: Input,
+    num_rows: u16,
+    rows: Vec<String>,
+    rowoffset: u16,
+    coloffset: u16,
 }
 
 impl Terminal {
@@ -33,11 +40,32 @@ impl Terminal {
 
         let size = size().unwrap();
 
-        Terminal { term_buf: String::new(), size , cursor: Cursor { cx: 0, cy: 0 }, input: Input {}}
+        Terminal { term_buf: String::new(), size , cursor: Cursor { cx: 0, cy: 0 }, input: Input {}, num_rows:0, rows: Vec::new(), rowoffset: 0, coloffset: 0}
         
     }
 
+    pub fn open_empty_editor(&mut self) {
+        self.rows = vec!["Hello World".to_string()];
+        self.num_rows = 0;
+    }
+
+    pub fn open_editor(&mut self, filename: String) -> Result<(), Box<dyn Error>> {
+        let file = File::open(filename)?;
+        let reader = io::BufReader::new(file);
+        
+        for line in reader.lines() {
+            let row = line?;
+            self.rows.push(row);
+        }
+
+        self.num_rows = self.rows.len() as u16;
+
+        Ok(())
+    }
+
     fn refresh_screen(&mut self) {
+        self.editor_scroll();
+
         self.term_buf.clear();
         self.term_buf.push_str("\x1b[?25l");
         self.term_buf.push_str("\x1b[H");
@@ -46,7 +74,7 @@ impl Terminal {
         
 
         // execute!(io::stdout(), MoveTo(self.cursor.cx, self.cursor.cy)).unwrap();
-        self.term_buf.push_str(& format!("\x1b[{};{}H", self.cursor.cx+1, self.cursor.cy+1));
+        self.term_buf.push_str(& format!("\x1b[{};{}H", (self.cursor.cx - self.rowoffset) + 1, (self.cursor.cy - self.coloffset) + 1));
         
         // Comment to not restore cursor to 0,0
         // self.term_buf.push_str("\x1b[H");
@@ -63,7 +91,7 @@ impl Terminal {
             // clear_screen();
  
             let key = self.input.handle_input();
-            self.move_cursor(key);
+            self.handle_input(key);
 
             self.refresh_screen();
             // fill_row('~', self.size.1);
@@ -73,48 +101,127 @@ impl Terminal {
         }
     }
 
+    fn editor_scroll(&mut self) {
+
+        if self.cursor.cx < self.rowoffset {
+            self.rowoffset = self.cursor.cx;
+        }
+        if self.cursor.cx >= self.rowoffset + self.size.1 {
+            self.rowoffset = self.cursor.cx - self.size.1 + 1;
+        }
+
+        if self.cursor.cy < self.coloffset {
+            self.coloffset = self.cursor.cy;
+        }
+        if self.cursor.cy >= self.coloffset + self.size.0 {
+            self.coloffset = self.cursor.cy - self.size.0 + 1;
+        }
+    }
+
     fn fill_row(&mut self, c: char) {
         for i in 0..self.size.1 {
+            let filerow = i + self.rowoffset;
+            if filerow >= self.num_rows {
+                let welcome = "Next Version 1.0".to_string();
 
-            let welcome = "Next Version 1.0".to_string();
+                if self.num_rows == 0 && i == self.size.1 / 3 {
+                    let mut padding = String::new();
+                    padding.push(c);
+                    for _ in 0..((self.size.0 / 2) - (welcome.len() as u16 / 2)) - 1 {
+                        padding.push(' ');
+                    }
+                    self.term_buf.push_str(&padding);
+                    self.term_buf.push_str(&welcome);
 
-            if i == self.size.1 / 3 {
-                let mut padding = String::new();
-                padding.push(c);
-                for _ in 0..((self.size.0 / 2) - (welcome.len() as u16 / 2)) - 1 {
-                    padding.push(' ');
+                } else {
+                    self.term_buf.push(c);
                 }
-                self.term_buf.push_str(&padding);
-                self.term_buf.push_str(&welcome);
-
             } else {
-                self.term_buf.push(c);
-            }
+                let current_line = &self.rows[filerow as usize];
 
-            
+                if current_line.len() > self.size.0.into() {
+                    self.term_buf.push_str(&current_line[self.coloffset as usize..(self.coloffset + self.size.0 - 1).into()]);
+                } else {
+                    self.term_buf.push_str(current_line);
+                }
+
+            }
+                
             self.term_buf.push_str("\x1b[K");
             if i < self.size.1 - 1 {
                 self.term_buf.push_str("\r\n");
             } 
+
         }
     }
 
     fn move_cursor(&mut self, key: Keys) {
+        let row = if self.cursor.cx >= self.num_rows {
+          None
+        } else {
+            Some(&self.rows[self.cursor.cx as usize])
+        };
+
+        match key {
+            Keys::Left => if self.cursor.cy != 0 {
+                self.cursor.cy -= 1
+            } else if self.cursor.cx > 0 {
+                    self.cursor.cx -= 1;
+                    self.cursor.cy = self.rows[self.cursor.cx as usize].len() as u16;
+                },
+            Keys::Down => if self.cursor.cx < self.num_rows {self.cursor.cx += 1},
+            Keys::Up => if self.cursor.cx != 0 {self.cursor.cx -= 1},
+            Keys::Right => if let Some(row) = row {
+                if self.cursor.cy < row.len() as u16 {
+                    self.cursor.cy += 1;
+                } else if self.cursor.cy == row.len() as u16 {
+                    self.cursor.cx += 1;
+                    self.cursor.cy = 0;
+                }
+                    
+                },
+            _ => ()
+        }
+
+        let row = if self.cursor.cx >= self.num_rows {
+          None
+        } else {
+            Some(&self.rows[self.cursor.cx as usize])
+        };
+        
+        let rowlen = if let Some(row) = row { row.len() } else { 0 } as u16;
+        if self.cursor.cy > rowlen {
+            self.cursor.cy = rowlen;
+        }
+
+    }
+
+    fn handle_input(&mut self, key: Keys) {
         match key {
             Keys::Char(c) => match c {
-                'h' => if self.cursor.cy != 0 {self.cursor.cy -= 1},
-                'j' => if self.size.1 - 1 != self.cursor.cx {self.cursor.cx += 1},
-                'k' => if self.cursor.cx != 0 {self.cursor.cx -= 1},
-                'l' => if self.size.0 - 1 != self.cursor.cy {self.cursor.cy += 1},
+                'h' => self.move_cursor(Keys::Left),
+                'j' => self.move_cursor(Keys::Down),
+                'k' => self.move_cursor(Keys::Up),
+                'l' => self.move_cursor(Keys::Right),
                 _ => ()
             }
-            Keys::Left => if self.cursor.cy != 0 {self.cursor.cy -= 1},
-            Keys::Down => if self.size.1 - 1 != self.cursor.cx {self.cursor.cx += 1},
-            Keys::Up => if self.cursor.cx != 0 {self.cursor.cx -= 1},
-            Keys::Right => if self.size.0 - 1 != self.cursor.cy {self.cursor.cy += 1},
+            Keys::Left => self.move_cursor(Keys::Left),
+            Keys::Down => self.move_cursor(Keys::Down),
+            Keys::Up => self.move_cursor(Keys::Up),
+            Keys::Right => self.move_cursor(Keys::Right),
             
             Keys::Home => self.cursor.cy = 0,
             Keys::End => self.cursor.cy = self.size.0 - 1,
+            Keys::PageUp => {
+                for _ in 0..self.size.1 - 1 {
+                    self.move_cursor(Keys::Up);
+                }
+            },
+            Keys::PageDown => {
+                for _ in 0..self.size.1 - 1 {
+                    self.move_cursor(Keys::Down);
+                }
+            },
             _ => ()
         }
     }
